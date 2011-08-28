66962758d3c4cf1caf158c8506e87869e4b03c08
martineq@gmail.com <martineq@gmail.com@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-08-28 17:34:16 +0000

    - Primer revisión. Agregada carpeta "src". Agregado main.cpp.-

---
+++ trunk/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    int i;
+
+    cout<<"Hola Mundo!!!\n";
+
+    for (i=0;i<argc;i++){  // El argv[0] es la ruta del binario.
+        cout<<"\nParámetro ["<<i<<"]: "<<argv[i];
+    }
+
+    cout<<"\n\nBueno, chau...\n";
+
+    return 0;
+}
