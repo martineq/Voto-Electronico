f2200faf2a84a216851d6b7bd7570ff06931b073
martineq <martineq@gmail.com>
2011-08-30 03:25:45 +0000

    - Actualización de carpetas.-

---
+++ TP0/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    int i;
+
+    cout<<"||TP 0||\n--------\n\nHola Mundo!!!\n";
+
+    for (i=0;i<argc;i++){
+        cout<<"\nParámetro ["<<i<<"]: "<<argv[i];
+    }
+
+    cout<<"\n\nBueno, chau...\n";
+
+    return 0;
+}
