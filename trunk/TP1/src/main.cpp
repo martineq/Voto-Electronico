dfaa721b9a3403e0e1a6de48ff4ff97aade8d66b
incorporado <incorporado@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-09-01 13:44:50 +0000

    Agregue una linea de cout

--- trunk/TP1/src/main.cpp
+++ trunk/TP1/src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc,char* argv[])
     int i;
 
     cout<<"||TP 1||\n--------\n\nHola Mundo!!!\n";
+    cout << "Daniel Shlufman padron 88040 - Saludos!" << endl;
 
     for (i=0;i<argc;i++){
         cout<<"\nParámetro ["<<i<<"]: "<<argv[i];
