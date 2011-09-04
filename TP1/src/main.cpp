77057c1e9169f786bc82f91fbd6146ecca38258a
martineq <martineq@gmail.com>
2011-09-04 16:36:15 +0000

    - Actualización main.-

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc,char* argv[])
 
     cout<<"||TP 1||\n--------\n\nHola Mundo!!!\n";
     cout << "Daniel Shlufman padron 88040 - Saludos!" << endl;
+    cout << "Martín Eduardo Quiroz padron 86012 - Saludos!" << endl;
 
     for (i=0;i<argc;i++){
         cout<<"\nParámetro ["<<i<<"]: "<<argv[i];
