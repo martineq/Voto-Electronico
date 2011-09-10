b6f2d5752958c0758aea439b30d01ff9e3ece8fa
martineq <martineq@gmail.com>
2011-09-10 04:20:29 +0000

    - Actualización de carpetas.-

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    int i;
+
+    cout<<"||TP 1||\n--------\n\nHola Mundo!!!\n";
+
+    cout<<"Si les llegó este commit al mail es porque lo configuré bien.\n";
+
+    cout << "Daniel Shlufman padron 88040 - Saludos!" << endl;
+
+    cout << "Martín Eduardo Quiroz padron 86012 - Eclipse" << endl;
+
+
+    for (i=0;i<argc;i++){
+        cout<<"\nParámetro ["<<i<<"]: "<<argv[i];
+    }
+
+    cout<<"\n\nBueno, chau...\n";
+
+    return 0;
+}
