34b95523143875707b5858834320e8d2fdf3f1da
martineq <martineq@gmail.com>
2011-09-17 22:45:55 +0000

    Entidad Candidato

---
+++ src/Entidades/Candidato.cpp
@@ -0,0 +1,17 @@
+/*
+ * Candidato.cpp
+ *
+ *  Created on: 17/09/2011
+ *      Author: mart
+ */
+
+#include "Candidato.h"
+
+Candidato::Candidato() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Candidato::~Candidato() {
+	// TODO Auto-generated destructor stub
+}
