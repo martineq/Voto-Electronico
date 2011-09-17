18e9c878e56738a8a29f16e1978d03a1f570bb32
incorporado <incorporado@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-09-17 22:24:23 +0000

    Clase Votante

---
+++ trunk/src/Entidades/EleccionAnterior.cpp
@@ -0,0 +1,18 @@
+#include "EleccionAnterior.h"
+
+EleccionAnterior::EleccionAnterior(string fecha, string cargo) {
+	this->fecha=fecha;
+	this->cargo=cargo;
+}
+
+EleccionAnterior::~EleccionAnterior() {
+
+}
+
+string EleccionAnterior::getFecha () {
+	return this->fecha;
+}
+
+string EleccionAnterior::getCargo () {
+	return this->cargo;
+}
---
+++ trunk/src/Entidades/EleccionAnterior.h
@@ -0,0 +1,19 @@
+#ifndef ELECCIONANTERIOR_H_
+#define ELECCIONANTERIOR_H_
+
+#include <iterator>
+#include <string>
+using namespace std;
+
+class EleccionAnterior {
+private:
+	string fecha;
+	string cargo;
+public:
+	EleccionAnterior(string fecha, string cargo);
+	string getFecha ();
+	string getCargo ();
+	virtual ~EleccionAnterior();
+};
+
+#endif /* ELECCIONANTERIOR_H_ */
