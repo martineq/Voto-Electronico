8922536fcdcfb7e137ad3a32c772fe51b6df3e9c
martineq@gmail.com <martineq@gmail.com@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-09-17 23:06:00 +0000

    Cambios en Candidato.-

--- trunk/src/Entidades/Candidato.cpp
+++ trunk/src/Entidades/Candidato.cpp
@@ -7,11 +7,14 @@
 
 #include "Candidato.h"
 
+
+
 Candidato::Candidato() {
-	// TODO Auto-generated constructor stub
 
 }
 
+
+
 Candidato::~Candidato() {
-	// TODO Auto-generated destructor stub
+
 }
--- trunk/src/Entidades/Candidato.h
+++ trunk/src/Entidades/Candidato.h
@@ -7,11 +7,25 @@
 
 #ifndef CANDIDATO_H_
 #define CANDIDATO_H_
+//Candidato ( ( (lista)ie, (votante)ie, (cargo)ie)i )
+//Candidato ( (fecha),(cargo),(nombre),(dni), (cargo) )
+
+#include <string>
+using namespace std;
+
 
 class Candidato {
+private:
+	string fecha;
+	string cargo;
+	string nombre;
+	int dni;
+
+
 public:
 	Candidato();
 	virtual ~Candidato();
+
 };
 
 #endif /* CANDIDATO_H_ */
