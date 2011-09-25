ca96e01168ecb0e1dbeec002cf205034d7ea794a
martinhernangomez@gmail.com <martinhernangomez@gmail.com@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-09-25 00:20:15 +0000

    ArchivoDeBuckets

---
+++ trunk/src/Archivos/Hashing/ArchivoDeBuckets.cpp
@@ -0,0 +1,31 @@
+#include "ArchivoDeBuckets.h"
+
+ArchivoDeBuckets::ArchivoDeBuckets() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ArchivoDeBuckets::ArchivoDeBuckets(string* path, int tamanioBucket)
+{
+}
+
+Bucket *ArchivoDeBuckets::obtenerBucket(int numeroDeBucket)
+{
+}
+
+int ArchivoDeBuckets::crearNuevoBucket()
+{
+}
+
+void ArchivoDeBuckets::liberarBucket(int numeroBucket)
+{
+}
+
+void ArchivoDeBuckets::guardarBucket(int numeroBucket, Bucket* bucket)
+{
+}
+
+ArchivoDeBuckets::~ArchivoDeBuckets() {
+	// TODO Auto-generated destructor stub
+}
+
