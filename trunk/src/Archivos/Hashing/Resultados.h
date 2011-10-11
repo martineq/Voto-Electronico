5a686524e072c8903aa5e8632a9733eae60b65bd
martinhernangomez@gmail.com <martinhernangomez@gmail.com@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-10-11 05:59:38 +0000

    ArchivoDeBuckets

--- trunk/src/Archivos/Hashing/Resultados.h
+++ trunk/src/Archivos/Hashing/Resultados.h
@@ -2,10 +2,11 @@
 #define RESULTADOS_H_
 
 enum Resultados {
-	  operacionOK = 0,
-	  bucketOcupado = 1,
-	  bucketLibre = 2,
-	  bucketNoDisponible = 3
+	  bucketOcupado 	= -1,
+	  bucketLibre 		= -2,
+	  bucketNoDisponible= -3,
+	  bucketNULO		= -4,
+	  operacionOK 		= -5
 	};
 
 #endif
