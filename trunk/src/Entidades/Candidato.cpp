acafe16622973e90ac7d39a7538f3d10606b6c07
martinhernangomez@gmail.com <martinhernangomez@gmail.com@363af2b4-d818-2680-8192-28fd2fea8ca9>
2011-10-15 23:15:26 +0000

    Un poco raro el Candidato, tengo la impresión de que le faltan un par de cosas..

--- trunk/src/Entidades/Candidato.cpp
+++ trunk/src/Entidades/Candidato.cpp
@@ -27,70 +27,31 @@ Candidato::~Candidato() {
 }
 
 string* Candidato::serializar() {
-	stringstream buffer;
-	int cantidadDeBytes;
-//	cantidadDeBytes   = this->fecha.size();
-//	buffer.write((char*)&cantidadDeBytes,TAM_INT);
-//	buffer.write((char*)this->fecha.c_str(),cantidadDeBytes);
-	buffer.write((char*)this->fecha.c_str(),TAM_FECHA);
-	cantidadDeBytes   = this->cargo.size();
-	buffer.write((char*)&cantidadDeBytes,TAM_INT);
-	buffer.write((char*)this->cargo.c_str(),cantidadDeBytes);
-	cantidadDeBytes   = this->nombre.size();
-	buffer.write((char*)&cantidadDeBytes,TAM_INT);
-	buffer.write((char*)this->nombre.c_str(),cantidadDeBytes);
-	buffer.write((char*)&this->dni,TAM_INT);
-	string* datos = new string(buffer.str());
-	return datos;
+	int fechaInt;
+	Serializadora serializadora;
+
+	stringstream fechaStream (fecha);
+	fechaStream >> fechaInt;
+
+	serializadora.agregarInt(fechaInt);
+	serializadora.agregarString(&cargo);
+	serializadora.agregarString(&nombre);
+	serializadora.agregarInt(dni);
+
+	return serializadora.obtenerSerializacion();
 }
 
 void Candidato::deserializar(string* source) {
-	istringstream buffer (*source);
-	stringstream * miString;
-	delete source;
-	int cantidadDeBytes;
 
-//	hidrato la fecha
-//	buffer.read((char*)&cantidadDeBytes,TAM_INT);
-//	char* fechaSerializada = new char[cantidadDeBytes];
-//	buffer.read((char*)fechaSerializada,cantidadDeBytes);
-	char* fechaSerializada = new char[TAM_FECHA];
-	buffer.read((char*)fechaSerializada,TAM_FECHA);
+	Serializadora serializadora(source);
 
-	miString = new stringstream();
-	miString->write(fechaSerializada,TAM_FECHA);
-	string* pasoAString = new string (miString->str());
-	this->fecha = *pasoAString;
-	delete []fechaSerializada;
-	delete pasoAString;
-	delete miString;
+	stringstream fechaStream;
+	fechaStream << serializadora.obtenerInt();
 
-//  hidrato el cargo principal
-	buffer.read((char*)&cantidadDeBytes,TAM_INT);
-	char* cargoSerializado = new char[cantidadDeBytes];
-	buffer.read((char*)cargoSerializado,cantidadDeBytes);
-	miString = new stringstream();
-	miString->write(cargoSerializado,cantidadDeBytes);
-	pasoAString = new string (miString->str());
-	this->cargo = *pasoAString;
-	delete []cargoSerializado;
-	delete pasoAString;
-	delete miString;
-
-//	hidrato la nombre
-	buffer.read((char*)&cantidadDeBytes,TAM_INT);
-	char* nombreSerializado = new char[cantidadDeBytes];
-	buffer.read((char*)nombreSerializado,cantidadDeBytes);
-	miString = new stringstream();
-	miString->write(nombreSerializado,cantidadDeBytes);
-	pasoAString = new string (miString->str());
-	this->nombre = *pasoAString;
-	delete []nombreSerializado;
-	delete pasoAString;
-	delete miString;
-
-//	hidrato dni
-	buffer.read((char*)&this->dni,TAM_INT);
+	fecha 	= fechaStream.str();
+	cargo 	= serializadora.obtenerString();
+	nombre 	= serializadora.obtenerString();
+	dni 	= serializadora.obtenerInt();
 }
 
 void Candidato::verCandidato() {
@@ -101,24 +62,32 @@ void Candidato::verCandidato() {
 }
 
 int Candidato::getTamanio(){
-	int tamanioCargo = TAM_INT + this->cargo.size();
-	int tamanioDni = TAM_INT;
-	int tamanioFecha = TAM_FECHA;
-	int tamanioNombre = TAM_INT + this->nombre.size();
+	int tamanioFecha 	= TAM_INT;
+	int tamanioCargo 	= TAM_INT + cargo.size();
+	int tamanioNombre 	= TAM_INT + nombre.size();
+	int tamanioDni 		= TAM_INT;
+
+	cout << "** tamanio candidato : " << tamanioCargo+tamanioDni+tamanioFecha+tamanioNombre << endl;
+	cout << "** tamanio serializado: " << serializar()->size() << endl;
 	return (tamanioCargo + tamanioDni + tamanioFecha + tamanioNombre);
 }
 
 int Candidato::getClave(){
 	int c = 0;
-	for (int i=0; i<this->fecha.length(); i++) {
+	int size = fecha.length();
+
+	for (int i=0; i < size ; i++)
 		c += (int)this->fecha[i];
-	}
-	for (int i=0; i<this->cargo.length(); i++) {
+
+	size = cargo.length();
+	for (int i=0; i< size ; i++)
 		c += (int)this->cargo[i];
-	}
-	for (int i=0; i<this->nombre.length(); i++) {
+
+
+	size = nombre.length();
+	for (int i=0; i < size; i++)
 		c += (int)this->nombre[i];
-	}
+
 	return c;
 }
 
